7c7ffcd19f5f09281c3dff69ed516b5368fa01d6
Aleksei <morgangso@gmail.com>
2016-12-13 16:00:13 +0300

    del key

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8-*-
 
-key_google = "AIzaSyAqk7vE0vQDR3JItUPgFp6bcPqgJz8h8tI"  ## google key Spigeni
+key_google = ""  ## google key Spigeni
 
-TOKEN = '291192018:AAF4Ey3mPw3PySmyBxzuTzMrGjoHis7fB8w' ## telegram.me/VoiceSearch_Bot
+TOKEN = "" ## telegram.me/VoiceSearch_Bot
